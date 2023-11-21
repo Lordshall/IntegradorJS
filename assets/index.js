3f1fc9cc9481cc535a67822199e1905e0712f086
Jose <joserivas1085@gmail.com>
2023-11-21 00:30:57 -0300

    Aplicaciones de JavaScript

--- assets/index.js
+++ assets/index.js
@@ -2,7 +2,10 @@
 const productsContainer = document.querySelector(".products-container");
 //Boton Ver mas
 const showMoreBtn = document.querySelector(".btn-more");
-
+// Contenedor Categorias
+const categoriesContainer = document.querySelector(".categories");
+// El HTML Collection de todas las categorias
+const categoriesList = document.querySelectorAll(".category");
 // ###################### Log para renderizar #################
 
 // ######################## Log Ver mas #########################
@@ -48,18 +51,66 @@ const renderProducts = (productList) => {
 
 // ######################## Log. Ver mas #########################
 
+// Fun para sabaer si es el final de array
+const isLastIndexOf = () => {
+  return AppState.currentProductsIndex === AppState.productsLimit - 1;
+};
+
+// Fun para renderizar al presionar ver mas
+
 const showMoreArticles = () => {
   AppState.currentProductsIndex += 1;
 
   let { products, currentProductsIndex } = AppState;
 
   renderProducts(products[currentProductsIndex]);
+
+  if (isLastIndexOf()) {
+    showMoreBtn.classList.add("hidden");
+  }
+};
+
+// ############# Log Filtros ################
+//Fun cambiar estado de los botones Categorias
+const changeBtnActiveState = (selectedCategory) => {
+  const categories = [...categoriesList];
+  console.log(AppState);
+
+  categories.forEach((categoryBtn) => {
+    if (categoryBtn.dataset.category !== selectedCategory) {
+      categoryBtn.classList.remove("active");
+      return;
+    }
+    categoryBtn.classList.add("active");
+  });
+};
+
+// Fiun para cambiar estado de filtros activo
+const changeFilterState = (btn) => {
+  AppState.activeFilter = btn.dataset.category;
+  changeBtnActiveState(AppState.activeFilter);
+};
+
+// Fiun para aplicar filtros
+const applyFilter = ({ target }) => {
+  if (!isInactiveFilterBtn(target)) return;
+  changeFilterState(target);
+};
+
+//Fun para saber si el elemento presionado es un
+//boton de categoria y no esta activo
+const isInactiveFilterBtn = (Element) => {
+  return (
+    Element.classList.contains("category") &&
+    !Element.classList.contains("active")
+  );
 };
 
 // Funcion Inic.
 const init = () => {
   renderProducts(AppState.products[0]);
   showMoreBtn.addEventListener("click", showMoreArticles);
+  categoriesContainer.addEventListener("click", applyFilter);
 };
 
 init();
